ff90ce8bdb4c70b58d2fbd2f8247dc8d32e5124b
pannaga3009 <pannaga3009@gmail.com>
2024-07-14 23:59:19 -0400

    changing settings

--- backend/recipe_backend/settings.py
+++ backend/recipe_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-an06_%x6@gfglz9qys4n0_#lopzpj*jorkcmem7e@tx87sycy8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.152.32.101', 'localhost']
 
 
 # Application definition
