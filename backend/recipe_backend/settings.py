44f1b93ce4dede35134e28a61886bfbc52af7a65
pannaga3009 <pannaga3009@gmail.com>
2024-07-15 23:20:19 -0400

    adding s3 bucket

--- backend/recipe_backend/settings.py
+++ backend/recipe_backend/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Allowing React development server
-    'https://your-s3-bucket-name.s3.amazonaws.com',
+    'https://django-react-bkt-1.s3.amazonaws.com',
 ]
 
 MIDDLEWARE = [
